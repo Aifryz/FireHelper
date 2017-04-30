0ee6255c7219424c3bf99aa2b53a43b5616863d9
Robert Bicz <robal10@poczta.fm>
2017-04-30 17:20:19 +0200

    Update index.php

--- php/index.php
+++ php/index.php
@@ -16,42 +16,40 @@ function GMapCircle($Lat,$Lng,$Rad,$Detail=8)
     $points[] = array($pLat,$pLng);
   endfor;
 
-  /* create encoded polyline from the points */
   require_once('PolylineEncoder.php');
   $PolyEnc   = new PolylineEncoder($points);
   $EncString = $PolyEnc->dpEncode();
   return $EncString['Points'];
 
 }
-/* set some options */
-$MapLat    = $_GET["lat"]; // latitude for map and circle center
-$MapLng    = $_GET["lng"]; // longitude as above
-$MapRadius = 0.36;         // the radius of our circle (in Kilometres)
-$MapFill   = 'E85F0E';    // fill colour of our circle
-$MapBorder = '91A93A';    // border colour of our circle
-$MapWidth  = 640;         // map image width (max 640px)
-$MapHeight = 480;         // map image heigt (max 640px)
+
+$MapLat    = $_GET["lat"];
+$MapLng    = $_GET["lng"]; 
+$MapRadius = 0.36;      
+$MapFill   = 'E85F0E';  
+$MapBorder = '91A93A';    
+$MapWidth  = 640;         
+$MapHeight = 480;         
 $m1 =  $_GET["mark1"]; 
 $m2 =  $_GET["mark2"];
 $m3 =  $_GET["mark3"]; 
 $x  = $_GET['x'];
 $y  = $_GET['y'];
-$p = 0;
 $EncString3 = array();
-foreach ($x as &$value)
+$c = 0;
+foreach($x as $value)
 {
-    $EncString3[p] = GMapCircle($x[p], $y[p], $MapRadius);
-    $p = p + 1;
+    $EncString3[$c] = GMapCircle($x[$c], $y[$c], $MapRadius);
+    $c = $c + 1;
 }
-$url;
-for ($i = 0; $i < $p; $i++)
+$url = '';
+
+for ($i = 0; $i <$c; $i++)
 {
-    $url += '&path=fillcolor:0x'.$MapFill.'33%7Ccolor:0x'.$MapBorder.'00%7Cenc:'.$EncString3[i];
+    $url .= '&path=fillcolor:0x'.$MapFill.'33%7Ccolor:0x'.$MapBorder.'00%7Cenc:'.$EncString3[$i];
 }
 
-$EncString2 = GMapCircle($x[1], $y[1], $MapRadius);
 $EncString = GMapCircle($MapLat,$MapLng, $MapRadius);
 $MapAPI = 'http://maps.google.com.au/maps/api/staticmap?';
 $MapURL = $MapAPI.'center='.$MapLat.','.$MapLng.'&size='.$MapWidth.'x'.$MapHeight.'&maptype=roadmap&path=fillcolor:0x'.$MapFill.'33%7Ccolor:0x'.$MapBorder.'00%7Cenc:'.$EncString.'&sensor=false&zoom=13&markers='.$m1.','.$m2.$m3.$url;
 echo $MapURL;?>
-
